77f91c302aa6260f64193fc5e14997347f08ca2c
luckyneo12 <mobiartlucky@gmail.com>
2025-10-15 12:17:59 +0530

    tower

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const app = express();
 
 // Configure CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-  credentials: true,
+  origin: '*',
+  credentials: false,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
